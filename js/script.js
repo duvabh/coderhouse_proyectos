02e0d7cd264c8ff38d242436d56b92e4d69ad1d9
Duvan Palacio <duvan.palacioo@gmail.com>
2022-10-12 00:04:15 -0500

    Modificación enrutamiento y adición de icono

--- js/script.js
+++ js/script.js
@@ -15,3 +15,18 @@ function seleccionar(){
     document.getElementById("header_nav").classList ="";
     menuVisible = false
 }
+
+
+//Efecto animaciones skill
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distacia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distacia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("comunicacion");
+        habilidades[1].classList.add("trabajo");
+        habilidades[2].classList.add("dedicacion");
+        habilidades[3].classList.add("responsabilidad");
+        habilidades[4].classList.add("puntualidad");
+    }
+}
